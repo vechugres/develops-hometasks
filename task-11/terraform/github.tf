7343279532c96b0bf6464639f7b18dc59918b48b
Kirill Serguchev <101435762+vechugres@users.noreply.github.com>
2022-05-30 14:01:23 +0300

    Update github.tf

--- task-11/terraform/github.tf
+++ task-11/terraform/github.tf
@@ -1,6 +1,6 @@
 resource "github_repository_file" "file" {
     count = length(var.files)
-    repository = "devops-hometasks"
+    repository = "develops-hometasks"
     branch = "main"
     file = "task-11/${element(var.files, count.index)}"
     content = file("${var.path}/${element(var.files, count.index)}")
