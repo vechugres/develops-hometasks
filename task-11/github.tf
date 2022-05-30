8fe87aa9aa35f892e3bd8ac9a832319a773968f7
Kirill Serguchev <101435762+vechugres@users.noreply.github.com>
2022-05-30 14:00:24 +0300

    my_commit

---
+++ task-11/github.tf
@@ -0,0 +1,11 @@
+resource "github_repository_file" "file" {
+
+    count = length(var.files)
+    repository = "develops-hometasks"
+    branch = "main"
+    file = "task-11/${element(var.files, count.index)}"
+    content = file("${var.path}/${element(var.files, count.index)}")
+    commit_message = "my_commit"
+    overwrite_on_create = true
+
+}
